b02eca5b39e22fe13c9953f7ce8f4e3e56acc286
Joris Lamberty <joris.lamberty@t-online.de>
2023-12-21 11:08:14 +0100

    Put everything into functions

--- Windows/install.ps1
+++ Windows/install.ps1
@@ -5,50 +5,50 @@
 #It is not possible to download something to the standard folder, so we move to out homefolder
 cd C:\Users\SysOperator
 
-#Download a Script to install winget and execute it
-Write-Host "Downloading Winget Script and Executing it"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Eisdaemon/Bwinf-Bewertung/main/Windows/winget.ps1" -OutFile "C:\Users\SysOperator\winget.ps1"
-$PSScriptRoot
-& "$PSScriptRoot\winget.ps1"
-#Register Winget
-Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
-#Clean Up the Winget install script
-Write-Host "Downloading and Installing Chocolatey"
-Remove-Item C:\Users\SysOperator\winget.ps1
-#Install Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+function install_prerequisites {
 
-#The Debloat Script is downloaded and executed, if not done already
-Write-Host "Now we begin with debloating Windows"
-do {
-  $RunDebloat = Read-Host -Prompt "Have you already run the Debloat Script? (y/n)"
-} while ($RunDebloat -ne "y" -and $RunDebloat -ne "n")
-
-if ($RunDebloat -eq "n") {
-  Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Eisdaemon/Bwinf-Bewertung/main/Windows/debloat.ps1" -OutFile "C:\Users\SysOperator\debloat.ps1"
+  Write-Host "Downloading Winget Script and Executing it"
+  Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Eisdaemon/Bwinf-Bewertung/main/Windows/winget.ps1" -OutFile "C:\Users\SysOperator\winget.ps1"
   $PSScriptRoot
-  & "$PSScriptRoot\debloat.ps1"
-  Remove-Item C:\Users\SysOperator\debloat.ps1
+  & "$PSScriptRoot\winget.ps1"
+  #Register Winget
+  Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
+  #Clean Up the Winget install script
+  Write-Host "Downloading and Installing Chocolatey"
+  Remove-Item C:\Users\SysOperator\winget.ps1
+  #Install Chocolatey
+  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
 }
 
-#Check if Chris Titus tool was run for Optimizations, this should be (most of the time) only be used for Optimizations. Installs are handled later, depending on Laptop type.
-Write-Host "Now we execute the Chris Titus Tool. Installing Programms is handled later, but the Optimizations should be executed"
-Start-Sleep -Seconds 2.5
-do {
-  $RunChris = Read-Host -Prompt "Have you already run the Chris Titus Tool for Optimizations? You may have to restart the script after using it(y/n)"
-} while ($RunChris -ne "y" -and $RunChris -ne "n")
-
-if ($RunChris -eq "n") {
-  iwr -useb https://christitus.com/win | iex
-}
-
-#Now Windows should be sufficiently debloated, removing a lot of bullsit, diabsling telemetry and many stupid tasks
-#We now ask if the device is used for a pool Laptop, or a coworker. Depending on which, different accounts and software will be used
-do {
-  $WhatKind = Read-Host -Prompt "Is the Device for the Pool or a coworker (pool/coworker)"
-} while ($WhatKind -ne "pool" -and $WhatKind -ne "coworker")
-if ($WhatKind -eq "pool") {
+function deblaot {
   
+  Write-Host "Now we begin with debloating Windows"
+  do {
+    $RunDebloat = Read-Host -Prompt "Have you already run the Debloat Script? (y/n)"
+  } while ($RunDebloat -ne "y" -and $RunDebloat -ne "n")
+
+  if ($RunDebloat -eq "n") {
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Eisdaemon/Bwinf-Bewertung/main/Windows/debloat.ps1" -OutFile "C:\Users\SysOperator\debloat.ps1"
+    $PSScriptRoot
+    & "$PSScriptRoot\debloat.ps1"
+    Remove-Item C:\Users\SysOperator\debloat.ps1
+  }
+
+  #Check if Chris Titus tool was run for Optimizations, this should be (most of the time) only be used for Optimizations. Installs are handled later, depending on Laptop type.
+  Write-Host "Now we execute the Chris Titus Tool. Installing Programms is handled later, but the Optimizations should be executed"
+  Start-Sleep -Seconds 2.5
+  do {
+    $RunChris = Read-Host -Prompt "Have you already run the Chris Titus Tool for Optimizations? You may have to restart the script after using it(y/n)"
+  } while ($RunChris -ne "y" -and $RunChris -ne "n")
+
+  if ($RunChris -eq "n") {
+    iwr -useb https://christitus.com/win | iex
+  }
+}
+
+function install_pool {
+
   #For a Pool Laptop we use to accounts, besides the SysOperator, these are an Admin "bewertung" Account for the evaluation days of the BWINF and a user account, for something like the Girl Camps
 
   $UserPass = Read-Host -Prompt "Enter the Password for the User Account"
@@ -73,7 +73,9 @@ if ($WhatKind -eq "pool") {
   winget install -e --id Oracle.VirtualBox; winget install -e --id BlueJTeam.BlueJ; winget install -e --id MITMediaLab.Scratch.3; winget install -e --id TheDocumentFoundation.LibreOffice; winget install -e --id LibreWolf.LibreWolf; winget install -e --id Neovim.Neovim; winget install -e --id vim.vim; winget install -e --id JetBrains.PyCharm.Community.EAP; winget install -e --id JetBrains.IntelliJIDEA.Community; winget install -e --id Microsoft.WindowsTerminal; winget install -e --id Microsoft.VisualStudioCode; winget upgrade --all;
   #Legt eine Txt mit Infos über die Programme an
   winget install -e --id Spyder.Spyder; winget install -e --id Python.Python.3.11; winget install -e --id Anaconda.Anaconda3; winget install -e --id 7zip.7zip
-} elseif ($WhatKind -eq "coworker") {
+}
+
+function install_coworker {
 
   #Coworkers get an Adming Account too. The UserName should be the last Name, and the password should be the same as for the Server
   $UserName = Read-Host -Prompt "Enter the last the of the coworker"
@@ -95,7 +97,33 @@ if ($WhatKind -eq "pool") {
   } while ($Exists -ne "y" -and $Exists -ne "n")
   cd ..\..\users\SysOperator\Desktop; copy Server.lnk ..\..\Public\Desktop
   Write-Host "For CloudPBX 2.0 there is no convient way of installation and for Element you have to change the Account to the Coworkers Accout, as there is no System Wide Installation"
+
 }
+
+#Install Prerequisites
+
+install_prerequisites
+
+#The Debloat Script is downloaded and executed, if not done already
+
+deblaot
+
+#Now Windows should be sufficiently debloated, removing a lot of bullsit, diabsling telemetry and many stupid tasks
+#We now ask if the device is used for a pool Laptop, or a coworker. Depending on which, different accounts and software will be used
+do {
+  $WhatKind = Read-Host -Prompt "Is the Device for the Pool or a coworker (pool/coworker)"
+} while ($WhatKind -ne "pool" -and $WhatKind -ne "coworker")
+if ($WhatKind -eq "pool") {
+
+  install_pool
+  
+} elseif ($WhatKind -eq "coworker") {
+
+  install_coworker
+
+}
+
+#Now we can finish up and cleanup
 
 Write-Host "Finished Install Script for Windows"
 Set-ExecutionPolicy -ExecutionPolicy AllSigned
