d17a09f7ea8bec8f6705130ac1e7232c20c0465f
Joris Lamberty <joris.lamberty@t-online.de>
2024-04-29 15:11:24 +0200

    Removed a few snaps

--- Linux/install.sh
+++ Linux/install.sh
@@ -16,19 +16,28 @@ read bewertungPass
 usermod -aG root bewertung
 
 
-apt-get update --fix-missing
-apt install kate okular gnome-boxes vim neovim visualvm codeblocks valgrind ddd emacs geany joe kdevelop gdb gcc nano konsole firefox gnome-terminal
-apt-get install ruby-full byobu
-snap install pycharm-community --classic
-snap install intellij-idea-community --classic
-snap install code --classic
+
+#VS Code
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
 snap install atom --classic
-snap install --classic eclipse
+
+#Eclipse
+wget https://www.eclipse.org/downloads/download.php?project=egit&file=eclipse-inst-jar
+tar -xvf eclipse-inst-jar
+cd eclipse-inst-jar && ./eclipse-inst
+
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-apt-get update
-apt-get install apt-transport-https
-apt-get install sublime-text
+apt-get update --fix-missing
+apt-get install
+apt-get install sublime-text apt-transport-https
+apt install kate okular gnome-boxes vim neovim visualvm codeblocks valgrind ddd emacs geany joe kdevelop gdb gcc nano konsole firefox gnome-terminal code
+apt-get install ruby-full byobu
+
 apt update && sudo apt upgrade
 
 
@@ -38,6 +47,7 @@ chmod +x ip.sh
 
 #Mädchenworkshop:
 #Linux:
+snap install pycharm-community --classic
 wget https://repo.anaconda.com/archive/Anaconda3-2023.03-1-Linux-x86_64.sh
 chmod +x  Anaconda3-2023.03-1-Linux-x86_64.sh
 bash Anaconda3-2023.03-1-Linux-x86_64.sh
