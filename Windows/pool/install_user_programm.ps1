0b7390679ee7ea0ac1d0b958db2d5289da958403
Joris Lamberty <joris.lamberty@t-online.de>
2025-12-15 13:38:47 +0100

    Test other install method for users

--- Windows/pool/install_user_programm.ps1
+++ Windows/pool/install_user_programm.ps1
@@ -1,18 +1,1 @@
-$taskName = "WingetInstallVSCodeAnderes"
-$wingetPath = "C:\Users\SysOperator\AppData\Local\Microsoft\WindowsApps\winget.exe"
-$arguments = "install Microsoft.VisualStudioCode3"
-$username = "anderes"
-$password = Read-Host "Enter password for $username" -AsSecureString
-$credential = New-Object System.Management.Automation.PSCredential($username, $password)
-
-# Create the action
-$action = New-ScheduledTaskAction -Execute $wingetPath -Argument $arguments
-
-# Create the principal with the correct logon type
-$principal = New-ScheduledTaskPrincipal -UserId "$env:COMPUTERNAME\$username" -LogonType Password -RunLevel Highest
-
-# Register the task with the credentials
-Register-ScheduledTask -TaskName $taskName -Action $action -Principal $principal -Settings (New-ScheduledTaskSettingsSet -StartWhenAvailable -DontStopOnIdleEnd) -Force
-
-# Run the task immediately
-Start-ScheduledTask -TaskName $taskName
+Invoke-WebRequest https://download.sysinternals.com/files/PsExec.zip -OutFile $env:TEMP\psexec.zip; Expand-Archive $env:TEMP\psexec.zip $env:TEMP\psexec -Force; Copy-Item $env:TEMP\psexec\PsExec.exe C:\Windows\System32 -Force; psexec -accepteula
